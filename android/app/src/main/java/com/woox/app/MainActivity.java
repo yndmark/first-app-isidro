2cc74a4253f79801f74b2d763d24a0c6bfa20ebc
yndmark <teamsteam28@gmail.com>
2025-04-04 18:28:22 +0800

    first application

---
+++ android/app/src/main/java/com/woox/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.woox.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.woox.app',
+  appName: 'Woox',
+  webDir: 'www'
+};
+
+export default config;
