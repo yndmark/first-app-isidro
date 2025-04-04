1e653c564df663013e8b9571f004c1e620170bfa
yndmark <119883891+yndmark@users.noreply.github.com>
2025-04-04 18:05:29 +0800

    Delete smpproj1 directory

--- smpproj1/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.woox.app',
-  appName: 'Woox',
-  webDir: 'www'
-};
-
-export default config;
